8b1f688dff60fc2d04a2ed46f3c0d8fea0919743
earayu <earayu@163.com>
2015-10-21 00:29:37 +0800

    加权有向图最短路径问题

---
+++ ShortestPath/EdgeWeightDiGraph.java
@@ -0,0 +1,39 @@
+package ShortestPath;
+
+import MST.Edge;
+import list.MyArrayList;
+
+public class EdgeWeightDiGraph {
+
+	private final int V;
+	private int E;
+	private MyArrayList<DirectedEdge>[] adj;
+	
+	public EdgeWeightDiGraph(int V) {
+		this.V = V;
+		this.E = 0;
+		adj = (MyArrayList<DirectedEdge>[]) new MyArrayList[this.V];
+		for(int i=0;i<this.V;i++)
+			adj[i] = new MyArrayList<DirectedEdge>();
+	}
+	
+	public int V(){
+		return this.V;
+	}
+	
+	public int E(){
+		return this.E;
+	}
+	
+	public void addEdge(DirectedEdge e){
+		adj[e.from()].add(e);
+		E++;
+	}
+	
+	public Iterable<DirectedEdge> adj(int v){
+		return adj[v];
+	}
+	
+
+
+}
